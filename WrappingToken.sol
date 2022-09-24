1b3d1594db13454e8b379b3bd10df733e90f5364
Konrad <konrad.strachan@gmail.com>
2022-09-24 23:04:55 +0100

    Made contract standalone

--- WrappingToken.sol
+++ WrappingToken.sol
@@ -1,6 +1,22 @@
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./IERC20.sol";
+// Borrowed from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol
+interface IERC20 {
+
+    event Transfer(address indexed from, address indexed to, uint amount);
+    event Approval(address indexed owner, address indexed spender, uint amount);
+
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+}
 
 contract WrappingToken is IERC20 {
     mapping(address => uint) public balances;
